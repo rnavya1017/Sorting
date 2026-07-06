c7c3102cc1e965f70b0af3f0384e0479c710f7f7
rnavya1017 <ravagunta.navya@gmail.com>
2026-07-06 17:25:39 +0530

    [Navya]:Selection sort program

---
+++ Selectionsort.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Selectionsort {
+
+    public static void selectionSort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        selectionSort(arr);
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+
+        sc.close();
+    }
+}
