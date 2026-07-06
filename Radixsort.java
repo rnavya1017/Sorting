bbf37a99e747ddee1170a9ae937027e58d792a23
rnavya1017 <ravagunta.navya@gmail.com>
2026-07-06 17:31:31 +0530

    [Navya]:Radix sort program

---
+++ Radixsort.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Radixsort {
+
+    static void countingSort(int arr[], int exp) {
+
+        int n = arr.length;
+
+        int output[] = new int[n];
+
+        int count[] = new int[10];
+
+        for (int i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    static void radixSort(int arr[]) {
+
+        int max = Arrays.stream(arr).max().getAsInt();
+
+        for (int exp = 1; max / exp > 0; exp *= 10)
+            countingSort(arr, exp);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        radixSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+        sc.close();
+    }
+}
